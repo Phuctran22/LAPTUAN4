d4b4e26c6f726f241d64a1ac0b990855129ee104
Phuctran22 <phuctran212005@gmail.com>
2025-03-17 16:43:23 +0700

    Main.java

--- BAI3/Main.java
+++ BAI3/Main.java
@@ -2,16 +2,33 @@ package BAI3;
 
 public class Main {
     public static void main(String[] args) {
-        circle c = new circle(1.0, "red", true);
-        System.out.println(c);
-        System.out.println("Area: " + c.getArea());
-        System.out.println("Perimeter: " + c.getPerimeter());
-        System.out.println();
 
-        square s = new square(2.0, "blue", false);
-        System.out.println(s);
-        System.out.println("Area: " + s.getArea());
-        System.out.println("Perimeter: " + s.getPerimeter());
-        System.out.println();
+        shape shape = new shape("blue", false);
+  
+        System.out.println(shape);
+
+        circle circle = new circle(2.0, "green", true);
+
+        System.out.println(circle);
+
+        System.out.println("Area: " + circle.getArea());
+
+        System.out.println("Perimeter: " + circle.getPerimeter());
+
+        rectangle rectangle = new rectangle(3.0, 4.0, "yellow", false);
+
+        System.out.println(rectangle);
+
+        System.out.println("Area: " + rectangle.getArea());
+
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+
+        square square = new square(2.5, "purple", true);
+
+        System.out.println(square);
+
+        System.out.println("Area: " + square.getArea());
+
+        System.out.println("Perimeter: " + square.getPerimeter());
     }
 }
